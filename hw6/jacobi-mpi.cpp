b8d777d5014731e4fbc6aa594ae0f54ffed7396d
Zhe Chen <chenzhepku@163.com>
2019-05-14 00:06:03 -0400

    debugged hw6

--- hw6/jacobi-mpi.cpp
+++ hw6/jacobi-mpi.cpp
@@ -1,8 +1,8 @@
 /* MPI-parallel Jacobi smoothing to solve -u''=f
- * Global vector has N unknowns, each processor works with its
- * part, which has lN = N/p unknowns.
- * Author: Georg Stadler
- */
+* Global vector has N unknowns, each processor works with its
+* part, which has lN = N/p unknowns.
+* Author: Georg Stadler
+*/
 #include <stdio.h>
 #include <math.h>
 #include <mpi.h>
@@ -16,7 +16,7 @@ double compute_residual(double *lu, int lN, double invhsq){
   for (i = 1; i <= lN; i++){
     for (j = 1; j<=lN; j++){
       tmp = (-lu[(lN+2)*j+i-1]-lu[(lN+2)*(j-1)+i]
-  		-lu[(lN+2)*j+i+1]-lu[(lN+2)*(j+1)+i]+4*lu[(lN+2)*j+i])*invhsq-1;
+      -lu[(lN+2)*j+i+1]-lu[(lN+2)*(j+1)+i]+4*lu[(lN+2)*j+i])*invhsq-1;
       lres += tmp * tmp;
     }
   }
@@ -42,12 +42,13 @@ int main(int argc, char * argv[]){
 
   sscanf(argv[1], "%d", &N);
   sscanf(argv[2], "%d", &max_iters);
+  int sp = (int)(sqrt(p));
 
   /* compute number of unknowns handled by each process */
-  lN = N / p;
-  if ((N % p != 0) && mpirank == 0 ) {
+  lN = N / sp;
+  if ((N % sp != 0) && mpirank == 0 ) {
     printf("N: %d, local N: %d\n", N, lN);
-    printf("Exiting. N must be a multiple of p\n");
+    printf("Exiting. N must be a multiple of sp\n");
     MPI_Abort(MPI_COMM_WORLD, 0);
   }
   /* timing */
@@ -59,16 +60,24 @@ int main(int argc, char * argv[]){
   double * lu    = (double *) calloc(sizeof(double), pow(lN + 2, 2));
   double * lunew = (double *) calloc(sizeof(double), pow(lN + 2, 2));
   double * lutemp;
+  double* send_left = (double *)calloc(sizeof(double),(lN));
+  double* recv_left = (double *)calloc(sizeof(double),(lN));
+  double* send_right = (double *)calloc(sizeof(double),(lN));
+  double* recv_right = (double *)calloc(sizeof(double),(lN));
+  double* send_up = (double *)calloc(sizeof(double),(lN));
+  double* recv_up = (double *)calloc(sizeof(double),(lN));
+  double* send_down = (double *)calloc(sizeof(double),(lN));
+  double* recv_down = (double *)calloc(sizeof(double),(lN));
 
   double h = 1.0 / (N + 1);
   double hsq = h * h;
   double invhsq = 1./hsq;
   double gres, gres0, tol = 1e-5;
 
+
   /* initial residual */
   gres0 = compute_residual(lu, lN, invhsq);
   gres = gres0;
-  int sp = (int)(sqrt(p));
 
   for (iter = 0; iter < max_iters && gres/gres0 > tol; iter++) {
 
@@ -80,29 +89,55 @@ int main(int argc, char * argv[]){
     }
 
     /* communicate ghost values */
+
     if ((mpirank % sp) < (sp - 1)) {
+      // left to right
       for (int j = 1; j <= lN; j++){
-      	MPI_Send(&(lunew[(lN+2)*j+lN]), 1, MPI_DOUBLE, mpirank+1, 124, MPI_COMM_WORLD);
-      	MPI_Recv(&(lunew[(lN+2)*j+lN+1]), 1, MPI_DOUBLE, mpirank+1, 123, MPI_COMM_WORLD, &status);
+        send_right[j-1]=lunew[(lN+2)*j+lN];
+      }
+      MPI_Send(send_right, lN, MPI_DOUBLE, mpirank+1, 124, MPI_COMM_WORLD);
+      MPI_Recv(recv_right, lN, MPI_DOUBLE, mpirank+1, 123, MPI_COMM_WORLD, &status);
+      for (int j = 1; j <= lN; j++){
+        lunew[(lN+2)*j+lN+1]=recv_right[j-1];
       }
     }
     if ((mpirank % sp) > 0) {
+      // right to left
       for (int j = 1; j <= lN; j++){
-      	MPI_Send(&(lunew[(lN+2)*j+1]), 1, MPI_DOUBLE, mpirank-1, 123, MPI_COMM_WORLD);
-      	MPI_Recv(&(lunew[(lN+2)*j]), 1, MPI_DOUBLE, mpirank-1, 124, MPI_COMM_WORLD, &status);
+        send_left[j-1]=lunew[(lN+2)*j+1];
+      }
+      MPI_Recv(recv_left, lN, MPI_DOUBLE, mpirank-1, 124, MPI_COMM_WORLD, &status);
+      MPI_Send(send_left, lN, MPI_DOUBLE, mpirank-1, 123, MPI_COMM_WORLD);
+
+      for (int j = 1; j <= lN; j++){
+        lunew[(lN+2)*j]=recv_left[j-1];
       }
     }
-    if (mpirank/sp > 0) {
-     for (int i=1; i <= lN; i++){
-      MPI_Send(&(lunew[lN+2+i]), 1, MPI_DOUBLE, mpirank-sp, 125, MPI_COMM_WORLD);
-      MPI_Recv(&(lunew[i]), 1, MPI_DOUBLE, mpirank-sp, 126, MPI_COMM_WORLD, &status1);
-     }
-    }
+
     if (mpirank/sp < sp-1) {
-     for (int i=1; i <= lN; i++){
-      MPI_Send(&(lunew[(lN+2)*lN+i]), 1, MPI_DOUBLE, mpirank+sp, 126, MPI_COMM_WORLD);
-      MPI_Recv(&(lunew[(lN+2)*(lN+1)+i]), 1, MPI_DOUBLE, mpirank+sp, 125, MPI_COMM_WORLD, &status1);
-     }
+      //down to up
+      for (int i = 1; i <= lN; i++){
+        send_up[i-1]=lunew[(lN+2)*lN+i];
+      }
+      MPI_Send(send_up, lN, MPI_DOUBLE, mpirank+sp, 125, MPI_COMM_WORLD);
+      MPI_Recv(recv_up, lN, MPI_DOUBLE, mpirank+sp, 126, MPI_COMM_WORLD, &status);
+      for (int i = 1; i <= lN; i++){
+        lunew[(lN+2)*(lN+1)+i]=recv_up[i-1];
+      }
+    }
+
+
+    if (mpirank/sp > 0) {
+      //up to down
+      for (int i = 1; i <= lN; i++){
+        send_down[i-1]=lunew[(lN+2)*1+i];
+      }
+      MPI_Recv(recv_down, lN, MPI_DOUBLE, mpirank-sp, 125, MPI_COMM_WORLD, &status);
+      MPI_Send(send_down, lN, MPI_DOUBLE, mpirank-sp, 126, MPI_COMM_WORLD);
+
+      for (int i = 1; i <= lN; i++){
+        lunew[(lN+2)*0+i]=recv_down[i-1];
+      }
     }
 
 
@@ -111,14 +146,23 @@ int main(int argc, char * argv[]){
     if (0 == (iter % 10)) {
       gres = compute_residual(lu, lN, invhsq);
       if (0 == mpirank) {
-	printf("Iter %d: Residual: %g\n", iter, gres);
+        printf("Iter %d: Residual: %g\n", iter, gres);
       }
     }
   }
 
+
   /* Clean up */
   free(lu);
   free(lunew);
+  free(send_right);
+  free(send_left);
+  free(send_down);
+  free(send_up);
+  free(recv_up);
+  free(recv_down);
+  free(recv_left);
+  free(recv_right);
 
   /* timing */
   MPI_Barrier(MPI_COMM_WORLD);
--- hw6/ssort.cpp
+++ hw6/ssort.cpp
@@ -11,35 +11,70 @@ int main( int argc, char *argv[]) {
   int rank, p;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
+  
+  //Debug mode!!!!!!!!!!!!!!!!!!!!!
+  // if (rank==0){
+  //   int i = 0;
+  //   char hostname[256];
+  //   gethostname(hostname, sizeof(hostname));
+  //   printf("PID %d on %s ready for attach\n", getpid(), hostname);
+  //   fflush(stdout);
+  //   while (0 == i)
+  //       sleep(5);
+  // }
+  // printf("Hello rank %d\n",rank );
 
   // Number of random numbers per processor (this should be increased
   // for actual tests or could be passed in through the command line
-  int N;
-  scanf(argv[1], "%d", &N);
+  int N=1024;
+  sscanf(argv[1], "%d", &N);
+
 
   int* vec = (int*)malloc(N*sizeof(int));
   // seed random number generator differently on every core
+
   srand((unsigned int) (rank + 393919));
 
   // fill vector with random integers
   for (int i = 0; i < N; ++i) {
-    vec[i] = rand();
+      vec[i] = rand();
   }
+
+  MPI_Barrier(MPI_COMM_WORLD);
   printf("rank: %d, first entry: %d\n", rank, vec[0]);
+
+  double tt = MPI_Wtime();
 
   // sort locally
   std::sort(vec, vec+N);
 
   // sample p-1 entries from vector as the local splitters, i.e.,
   // every N/P-th entry of the sorted vector
+  int* local_sp = (int*)malloc((p-1)*sizeof(int));
+  for (int i = 0; i < p-1; i++) {
+    local_sp[i] = vec[(i+1)*(N/p)-1];
+  }
 
   // every process communicates the selected entries to the root
   // process; use for instance an MPI_Gather
+  int* root_sp = NULL;
+  if (rank == 0){
+    root_sp = (int*)malloc(sizeof(int)*(p-1)*p);
+  }
+  MPI_Gather(local_sp,p-1,MPI_INT,root_sp,p-1,MPI_INT,0,MPI_COMM_WORLD);
 
   // root process does a sort and picks (p-1) splitters (from the
   // p(p-1) received elements)
+  int* global_sp = (int*)malloc((p-1)*sizeof(int));
+  if (rank == 0){
+    std::sort(root_sp,root_sp+(p-1)*p);
+    for (int i = 0; i < p-1; i++){
+      global_sp[i]=root_sp[i*p+(p-1)/2];
+    }
+  }
 
   // root process broadcasts splitters to all other processes
+  MPI_Bcast(global_sp,p-1,MPI_INT,0,MPI_COMM_WORLD);
 
   // every process uses the obtained splitters to decide which
   // integers need to be sent to which other process (local bins).
@@ -53,16 +88,66 @@ int main( int argc, char *argv[]) {
   // counts and displacements. For a splitter s[i], the corresponding
   // send-displacement for the message to process (i+1) is then given by,
   // sdispls[i+1] = std::lower_bound(vec, vec+N, s[i]) - vec;
+  int* sdispls = (int*)malloc((p)*sizeof(int));
+  int* sendcnts = (int*)malloc((p)*sizeof(int));
+  sdispls[0]=0;
+  for (int i=0; i < p-1; i++){
+  	sdispls[i+1]=std::lower_bound(vec, vec+N, global_sp[i])-vec;
+  	sendcnts[i] = sdispls[i+1]-sdispls[i];
+  }
+  sendcnts[p-1]=N-sdispls[p-1];
+
 
   // send and receive: first use an MPI_Alltoall to share with every
   // process how many integers it should expect, and then use
   // MPI_Alltoallv to exchange the data
+  int * recvcnts = (int*)malloc((p)*sizeof(int));
+  MPI_Alltoall(sendcnts, 1, MPI_INT,recvcnts, 1, MPI_INT,MPI_COMM_WORLD);//max recv number
+  int * rdispls = (int*) malloc(p*sizeof(int));
+  rdispls[0]=0;
+  for (int i=1; i < p; i++){
+  	rdispls[i]=rdispls[i-1]+recvcnts[i-1];
+  }
+  int local_size = rdispls[p-1]+recvcnts[p-1];//size of this recvbuf
+  int* recvbuf = (int*)malloc(local_size*sizeof(int));
+  MPI_Alltoallv(vec, sendcnts, sdispls, MPI_INT, recvbuf, recvcnts, rdispls, MPI_INT, MPI_COMM_WORLD);
 
   // do a local sort of the received data
+  std::sort(recvbuf,recvbuf+local_size);
 
   // every process writes its result to a file
+  MPI_Barrier(MPI_COMM_WORLD);
+  if (rank == 0){
+    printf("Total time is %f (s)\n",MPI_Wtime()-tt);
+  }
+  { // Write output to a file
+    FILE* fd = NULL;
+    char filename[256];
+    snprintf(filename, 256, "output%02d.txt", rank);
+    fd = fopen(filename,"w+");
 
+    if(NULL == fd) {
+      printf("Error opening file \n");
+      return 1;
+    }
+
+    fprintf(fd, "//rank %d\n", rank);
+    for(int i = 0; i < local_size; i++)
+      fprintf(fd, "%d\n", recvbuf[i]);
+
+    fclose(fd);
+  }
+
+  //free space
+  free(root_sp);
   free(vec);
+  free(global_sp);
+  free(sdispls);
+  free(sendcnts);
+  free(recvcnts);
+  free(rdispls);
+  free(recvbuf);
+  free(local_sp);
   MPI_Finalize();
   return 0;
 }
